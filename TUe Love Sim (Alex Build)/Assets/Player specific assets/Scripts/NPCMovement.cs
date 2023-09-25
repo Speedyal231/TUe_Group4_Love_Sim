290e4b69e515c7a3893f8f285b52da3d01c36575
Speedyal231 <144699723+Speedyal231@users.noreply.github.com>
2023-09-25 02:43:13 +0200

    NPC behaviour

--- TUe Love Sim (Alex Build)/Assets/Player specific assets/Scripts/NPCMovement.cs
+++ TUe Love Sim (Alex Build)/Assets/Player specific assets/Scripts/NPCMovement.cs
@@ -16,7 +16,38 @@ public class NPCMovement : MonoBehaviour
     [SerializeField] float acceleration;
     [SerializeField] float accelerationFriction;
     [SerializeField] float groundMaxSpeed;
+    Vector3 velocity;
 
+    //Variables that contribute to and store object collisions and raycast data
+    [Header("Ground Detection")]
+    [SerializeField] float rayOriginOffset;
+    [SerializeField] float groundHitRange;
+    [SerializeField] LayerMask groundLayer;
+    RaycastHit groundedRayPoint;
+    bool grounded;
+
+    [Header("Air Variables")]
+    [SerializeField] float gravity;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float airDrag;
+    [SerializeField] float airMaxSpeed;
+
+    private enum State
+    {
+        Ground,
+        Air
+    }
+    private State state;
+
+    /// <summary>
+    ///
+    /// 
+    /// 
+    /// Spacing between variable declarations and funtion declaration.
+    /// 
+    /// 
+    /// 
+    /// </summary>\
 
     // Start is called before the first frame update
     void Start()
@@ -25,8 +56,81 @@ public class NPCMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        PhysicsCalcInit();
+        GroundedCheck();
+        GroundSnap();
+        Turn();
+        Move();
+        Friction();
+        ApplyForces();
+    }
+    /// <summary>
+    ///
+    /// 
+    /// 
+    /// Spacing between main and general funtion declaration.
+    /// 
+    /// Fix weird  teleport bug
+    /// 
+    /// 
+    /// </summary>
+
+    private void PhysicsCalcInit()
+    {
+        velocity = Vector3.zero;
+    }
+
+    private void ApplyForces()
+    {
+        RB.velocity += velocity;
+    }
+
+    private void Turn() 
+    {
+        Vector3 NPCDirection = playerTransform.position - characterTransform.position;
+        NPCDirection = new Vector3(NPCDirection.x, 0, NPCDirection.z);
+        Quaternion rotate = Quaternion.LookRotation(NPCDirection, characterTransform.up);
+        characterTransform.rotation = rotate;
+    }
+
+    private void Move()
+    {
+        velocity += characterTransform.forward.normalized * acceleration;
+    }
+
+    private void Friction()
+    {
+        if (RB.velocity.magnitude != 0f)
+        {
+            if (RB.velocity.magnitude >= groundMaxSpeed)
+            {
+                velocity -= RB.velocity.normalized * acceleration;
+            }
+            else
+            {
+
+                if (RB.velocity.magnitude < accelerationFriction)
+                {
+                    velocity -= RB.velocity;
+                }
+                else
+                {
+                    velocity -= RB.velocity.normalized * accelerationFriction;
+                }
+
+            }
+        }
+    }
+
+    private void GroundSnap()
+    {
+        characterTransform.position = groundedRayPoint.point;
+    }
+
+    private void GroundedCheck()
+    {
+        grounded = Physics.Raycast(characterTransform.position + (characterTransform.up.normalized * rayOriginOffset), -characterTransform.up, out groundedRayPoint, groundHitRange + rayOriginOffset, groundLayer);
     }
 }
