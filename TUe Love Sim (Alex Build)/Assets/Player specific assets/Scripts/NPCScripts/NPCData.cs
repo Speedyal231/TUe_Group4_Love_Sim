c0ad7a8fdace366fb9c01834603aaf295fb51ad5
Speedyal231 <144699723+Speedyal231@users.noreply.github.com>
2023-09-29 18:11:50 +0200

    Player and NPC data structures set up

--- TUe Love Sim (Alex Build)/Assets/Player specific assets/Scripts/NPCScripts/NPCData.cs
+++ TUe Love Sim (Alex Build)/Assets/Player specific assets/Scripts/NPCScripts/NPCData.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 
 public class NPCData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Starting Stats")]
+    [SerializeField] float startHealth;
+    [SerializeField] float punchDamage;
+
+
+    private float health;
+
+    private void Start()
     {
-        
+        health = startHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 
+    /// Fetching functions
+    /// 
+    /// <returns></returns>
+    public float FetchHealth()
     {
-        
+        return health;
     }
+
+    public float PunchDamage()
+    {
+        return punchDamage;
+    }
+
+    /// <summary>
+    /// 
+    /// updating functions
+    /// 
+    /// <returns></returns>
+
+    public void ChangeHealth(float change)
+    {
+        health += change;
+    }
+
 }
--- TUe Love Sim (Alex Build)/Assets/Player specific assets/Scripts/PlayerScripts/PlayerData.cs
+++ TUe Love Sim (Alex Build)/Assets/Player specific assets/Scripts/PlayerScripts/PlayerData.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Starting Stats")]
+    [SerializeField] float startHealth;
+    [SerializeField] float punchDamage;
+    [SerializeField] float finisherDamage;
+    private float health;
+    private float combo;
+    private float targetRizzedCount;
+    RizzStatus rizzStatus;
+    public enum RizzStatus 
     {
-        
+        MegaMinger,
+        Minger,
+        Normie,
+        Rizzler,
+        RizzGod,
+        GigaChad
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        health = startHealth; 
+        combo = 0;
+        targetRizzedCount = 0;
+        rizzStatus = RizzStatus.MegaMinger;
     }
+
+
+    /// <summary>
+    /// 
+    /// Fetching functions
+    /// 
+    /// <returns></returns>
+    public float PunchDamage() 
+    {
+        return punchDamage;
+    }
+
+    public float FinisherDamage()
+    {
+        return finisherDamage;
+    }
+
+    public float FetchHealth() 
+    {
+        return health;
+    } 
+
+    public float FetchCombo() 
+    {
+        return combo;
+    }
+
+    public float FetchTargetRizzedCount() 
+    {
+        return targetRizzedCount;
+    }
+
+    public RizzStatus GetRizzStatus() 
+    { 
+        return rizzStatus;
+    }
+
+    /// <summary>
+    /// 
+    /// updating functions
+    /// 
+    /// <returns></returns>
+
+    public void ChangeHealth(float change) 
+    { 
+        health += change;
+    }
+
+    public void ChangeCombo(float change)
+    {
+        combo += change;
+    }
+
+    public void ChangeTargetRizzedCount(float change)
+    {
+        targetRizzedCount += change;
+    }
+
+
 }
